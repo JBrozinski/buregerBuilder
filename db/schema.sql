40e2dba8aa98ce7e2540af3dbd465e0f32f96393
JBrozinski <joseph.brozinski@gmail.com>
2020-05-10 15:28:55 -0600

    debugged ajax issues

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE burgerDB;
+USE burgerDB;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL
+	AUTO_INCREMENT,
+	name varchar
+	(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY
+	(id)
+);
